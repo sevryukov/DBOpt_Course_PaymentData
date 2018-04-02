9e392450678da99942aeba80a3b1991ea98976f8
koshreality <koshreality@gmail.com>
2018-04-02 23:25:00 +0300

    скрипт для подсчета баланса

---
+++ Tokarev_Kayumova/balance_computation.sql
@@ -0,0 +1,7 @@
+﻿-- SELECT SUM(p.Amount), c.FirstName, c.SecondName FROM Payment p, Client c WHERE c.oid = p.Payer GROUP BY c.FirstName, c.SecondName;
+SELECT t1.Plus - t2.Minus AS Balance, c.FirstName, c.SecondName FROM
+(SELECT SUM(p.Amount) AS Plus, p.Payee FROM Payment p GROUP BY p.Payee) t1,
+(SELECT SUM(p.Amount) AS Minus, p.Payer FROM Payment p GROUP BY p.Payer) t2,
+  Client c
+  WHERE t1.Payee = t2.Payer AND t1.Payee = c.Oid
+  ORDER BY c.SecondName;
