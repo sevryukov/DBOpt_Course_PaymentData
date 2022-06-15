41295898938e7e97d378f2e4af5ce61fcb308a8c
nyaneet <nyaneet@gmail.com>
2022-06-15 17:51:33 +0300

    deleted old directory

--- task_1/DeleteIndexes.sql
+++
@@ -1,5 +0,0 @@
-USE PaymentData
-
-DROP INDEX IF EXISTS PayCat_NotInPaymentProfit ON [dbo].[PaymentCategory]
-DROP INDEX IF EXISTS PayCat_Name ON [dbo].[PaymentCategory]
-DROP INDEX IF EXISTS AccType_Name ON [dbo].[AccountType]
